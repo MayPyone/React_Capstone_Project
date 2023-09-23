7b310815a51d16800cd5087bc1514674ad4c42a0
May Pyone <maypyone015@gmail.com>
2023-09-23 14:59:32 +0630

    Update tests for cuisinelist

--- src/component/__test__/CuisineList.test.js
+++ src/component/__test__/CuisineList.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import Test from './Test';
 import CuisineList from '../CuisineList';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter for NavLink
+
 
 jest.mock('react-redux', () => ({
   useDispatch: jest.fn(),
@@ -10,22 +13,20 @@ jest.mock('react-redux', () => ({
 describe('CuisineList Component', () => {
   it('renders all after fetching data', () => {
     const cuisine = {
+
       title: 'Cauliflower, Brown Rice, and Vegetable Fried Rice',
       image: 'img.jpg',
       id: 716426,
     };
-    const { getByText, getByTestId } = render(
-      <BrowserRouter>
-        <CuisineList
-          title={cuisine.title}
-          image={cuisine.image}
-          id={cuisine.id}
-        />
-      </BrowserRouter>,
-    );
+    
+    const {getByText,getByTestId } = render(
+      <Router>
+      <CuisineList title={cuisine.title } image={cuisine.image} id={cuisine.id}/>
+      </Router>
+  );
     const imageElement = screen.getByAltText('cuisine image');
-    expect(getByText('Cauliflower, Brown Rice, and Vegetable Fried Rice')).toBeInTheDocument();
-    expect(imageElement).toHaveAttribute('src', 'img.jpg');
-    expect(getByTestId('back-button')).toBeInTheDocument();
+     expect(getByText('Cauliflower, Brown Rice, and Vegetable Fried Rice')).toBeInTheDocument();
+     expect(imageElement).toHaveAttribute('src', 'img.jpg');
+     expect(getByTestId('back-button')).toBeInTheDocument();
   });
 });
